67b35afe6ba195a6cd7b10faf79560c798391288
Samit Koyom <samitkoyom@gmail.com>
2020-04-12 17:15:10 +0700

    update final project

--- src/app/pages/login-register/login-register.component.ts
+++ src/app/pages/login-register/login-register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-register',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginRegisterComponent implements OnInit {
 
-  constructor() { }
+  // ตัวแปร
+  message:string = "Hello Angular";
+
+  // ตัวแปร object
+  profile = {
+    "name":"Samit",
+    "tel":"08398938933",
+    "gender":"Male"
+  }
+
+  // 2 Way data binding
+  userData = {
+    "email":"",
+    "password":""
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    
+  }
+
+  submitLogin(){
+    // alert("Hey Angular");
+    if(this.userData.email == "admin@email.com" && this.userData.password == "1234")
+    {
+      // alert("Login Success");
+      this.router.navigate(['backend']);
+    }else{
+      alert("Login Fail!!!");
+    }
   }
 
 }
